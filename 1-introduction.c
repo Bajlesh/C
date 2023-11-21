37f460c06cf75e68de24e59747c9f7a61855dbda
Srajan Rai <xdsrajan@gmail.com>
2023-11-22 00:29:12 +0530

    Initial commit

---
+++ 1-introduction.c
@@ -0,0 +1,8 @@
+#include "header.h"
+
+int main() {
+    clear();
+    // printf("Hello World");
+    printHelloWorld();
+    return 0;
+}
---
+++ header.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+
+void clear() {
+    system("cls");
+}
+
+void printHelloWorld() {
+    printf("Hello World!\n");
+}
